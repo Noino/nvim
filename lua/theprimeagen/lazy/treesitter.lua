8566e0e5d7ee2f924c7c5d3457a750a19b822ec1
Noino <willewendell@gmail.com>
2025-01-28 13:29:02 +0200

    i'm becoming a lambo coder again

--- lua/theprimeagen/lazy/treesitter.lua
+++ lua/theprimeagen/lazy/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
-            ensure_installed = { "vimdoc", "javascript", "lua", "rust", "go", "perl", "bash" },
+            ensure_installed = { "vimdoc", "javascript", "lua", "rust", "go", "perl", "bash", "php" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
