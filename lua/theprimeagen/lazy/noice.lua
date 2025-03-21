31f59cebeecbb6623510815b8c21a88c0f243613
Noino <willewendell@gmail.com>
2025-03-21 12:28:42 +0200

    desperately trying to get select menus working :(

--- lua/theprimeagen/lazy/noice.lua
+++ lua/theprimeagen/lazy/noice.lua
@@ -10,12 +10,13 @@ return {
         "rcarriga/nvim-notify",
     },
     config = function()
-        vim.opt.showmode = false
+        require("telescope").load_extension("noice")
         local palette = require('rose-pine.palette')
+        vim.opt.showmode = false
+
         require("notify").setup({
             background_colour = palette.base,
-            render = "wrapped-default",
-            max_width = 75,
+            render = "default",
         })
 
         require("noice").setup({
@@ -28,14 +29,39 @@ return {
                 },
             },
 
-            cmdline = { enabled = true },
+            cmdline = { view = "cmdline" },
             -- you can enable a preset for easier configuration
             presets = {
                 bottom_search = true,         -- use a classic bottom cmdline for search
                 command_palette = true,       -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                inc_rename = true,            -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true,        -- add a border to hover docs and signature help
+            },
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        kind = "search_count",
+                    },
+                    opts = { skip = true },
+                },
+                {
+                    filter = {
+                        event = "msg_show",
+                        blocking = true,
+                    },
+                    view = "vsplit",
+                },
+                {
+                    filter = {
+                        event = "notify",
+                        find = "No information available"
+                    },
+                    opts = {
+                        skip = true
+                    },
+                },
             },
         })
     end
